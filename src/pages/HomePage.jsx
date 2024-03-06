e98ca520dc53081b27052acf5330ac9581a99e7a
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-03-06 16:22:48 +0100

    updated HomePage and PlantsList components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
+import "./HomePage.css"
 
 function HomePage() {
     return (
         <>
+            {/* <div className="w-full h-full bg-repeat"
+        style={{backgroundImage: url('../assets/plantBackground.img')}}>
+
+        </div> */}
+            <div className="HomePage">
+                <h1>Keep your <br />plants <br />alive</h1>
+                <p>Create your own and unique care plan specific <br />
+                    to every one of your plants taking into account <br />
+                    their own individual needs.</p>
+            </div>
         </>
     )
 }
--- src/pages/PlantList.jsx
+++ src/pages/PlantList.jsx
@@ -1,9 +1,102 @@
+import img1 from "../assets/plant1.jpg"
+import img2 from "../assets/plant2.jpg"
+import img3 from "../assets/plant3.jpg"
+import img4 from "../assets/plant4.jpg"
+import img5 from "../assets/plant5.jpg"
+import img6 from "../assets/plant6.jpg"
+import img7 from "../assets/plant7.jpg"
+import img8 from "../assets/plant8.jpg"
 
 function PlantList() {
     return (
-        <section className="my-20">
-            <h1 className="text-green-700 text-3xl font-bold text-center">Discover the beauty of our users plants</h1>
-        </section>
+        <div className="Plantlist">
+
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max " src={img1} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Monstera deliciosa</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max" src={img2} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Spider Plant</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max" src={img3} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">ZZ Plant</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max" src={img4} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Chinese Evergreen</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max" src={img5} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Iron Plant</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max" src={img6} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Fiddle Leaf Fig</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max" src={img7} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Aloe Vera</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-2xl transition duration-300 ease-in-out hover:scale-105">
+                <img className="w-full h-max" src={img8} />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Rubber Tree</div>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#indoor/outdoor</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#easy care</span>
+                    </div>
+            </div>
+        
+        </div>
+        // <section className="my-20">
+        //     <h1 className="text-green-700 text-3xl font-bold text-center">Discover the beauty of our users plants</h1>
+
+        // </section>
     )
 }
 
