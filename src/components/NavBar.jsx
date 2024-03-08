6a1b99915e17d79e34aefd822b142bea91203737
Lu Marconi <lumarconi21@gmail.com>
2024-03-08 12:31:30 +0100

    Make Navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,27 +10,23 @@ function Navbar() {
         logOutUser
     } = useContext(AuthContext);
     return (
-        <nav className="flex justify-between items-center mx-4 bg-green-700">
-
-
-            <div className="h-16 self-start"><img className="h-16" src={Logo} /> </div>
-            {isLoggedIn && (
-                <>
-                    <span className="text-sm text-white">Welcome {user && user.name}</span>
-                    <Link to="/logout" onClick={logOutUser} className="font-bold text-white">Logout</Link>
-                </>
-            )}
-
-            {!isLoggedIn && (
-                <>
-                    <NavLink to="/signup" className="font-bold text-white"> <button>Sign Up</button> </NavLink>
-                    <NavLink to="/login" className="font-bold text-white"> <button>Login</button> </NavLink>
-                </>
-            )}
-
-            <NavLink to="/" className="font-bold text-white">Home</NavLink>
-            <NavLink to="/plants" className="font-bold text-white">Plants</NavLink>
-
+        <nav className="flex justify-between items-center bg-green-700 lg:pr-4">
+            <div className="ml-1"><img className="h-10 lg:h-16" src={Logo} alt="Logo" /></div>
+            <div>
+                {isLoggedIn ? (
+                    <>
+                        <span className="text-xs text-white mx-4 lg:text-sm">Welcome {user && user.name}</span>
+                        <Link to="/logout" onClick={logOutUser} className="mx-2 text-xs font-semibold text-white lg:text-lg lg:font-bold">Logout</Link>
+                    </>
+                ) : (
+                    <>
+                        <NavLink to="/signup" className="mx-2 text-xs font-semibold text-white lg:text-lg lg:font-bold">Sign Up</NavLink>
+                        <NavLink to="/login" className="mx-2 text-xs font-semibold text-white lg:text-lg lg:font-bold">Login</NavLink>
+                    </>
+                )}
+                <NavLink to="/" className="mx-2 text-xs font-semibold text-white lg:text-lg lg:font-bold">Home</NavLink>
+                <NavLink to="/plants" className="mx-2 text-xs font-semibold text-white lg:text-lg lg:font-bold">Plants</NavLink>
+            </div>
         </nav>
     )
 }
