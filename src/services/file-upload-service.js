7b220b0b6299684ec58e2b114a5ff60b524437fa
Lu Marconi <lumarconi21@gmail.com>
2024-03-12 11:44:09 +0100

    Add redirect

--- src/services/file-upload-service.js
+++ src/services/file-upload-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:5005",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005",
   // withCredentials: true // => you might need this option if using cookies and sessions
 });
 
