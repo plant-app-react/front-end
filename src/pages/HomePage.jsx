9b0938f3a53f66e764442194e51922f117d7cc9a
Lu Marconi <lumarconi21@gmail.com>
2024-03-12 12:36:02 +0100

    Fix Homepage img routes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,9 @@
-
+import plant1 from "../assets/plant1.jpg"
+import plant2 from "../assets/plant2.jpg"
+import plant3 from "../assets/plant3.jpg"
+import plant4 from "../assets/plant4.jpg"
+import plant5 from "../assets/plant5.jpg"
+import plant6 from "../assets/plant6.jpg"
 
 function HomePage() {
     return (
@@ -13,12 +18,12 @@ function HomePage() {
                         and unlock your plant's full potential</p>
                 </div>
                 <div className="grid lg:grid-cols-3 gap-12 w-2/6 h-96">
-                    <img src="./src/assets/plant1.jpg" alt="plant" className="rounded-xl" />
-                    <img src="./src/assets/plant2.jpg" alt="plant" className="rounded-xl" />
-                    <img src="./src/assets/plant7.jpg" alt="plant" className="rounded-xl" />
-                    <img src="./src/assets/plant4.jpg" alt="plant" className="rounded-xl" />
-                    <img src="./src/assets/plant3.jpg" alt="plant" className="rounded-xl" />
-                    <img src="./src/assets/plant5.jpg" alt="plant" className="rounded-xl" />
+                    <img src={plant1} alt="plant" className="rounded-xl" />
+                    <img src={plant2} alt="plant" className="rounded-xl" />
+                    <img src={plant3} alt="plant" className="rounded-xl" />
+                    <img src={plant4} alt="plant" className="rounded-xl" />
+                    <img src={plant5} alt="plant" className="rounded-xl" />
+                    <img src={plant6} alt="plant" className="rounded-xl" />
 
                 </div>
             </div >
