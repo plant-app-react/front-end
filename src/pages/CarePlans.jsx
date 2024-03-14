288470f752740362c745033dae023c683a5cc41d
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-03-14 16:15:05 +0100

    finished CarePlans component with to does list

--- src/pages/CarePlans.jsx
+++ src/pages/CarePlans.jsx
@@ -1,13 +1,15 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import moment from 'moment';
+import "./CarePlans.css"
+import { Link } from "react-router-dom";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 const CarePlans = () => {
 
     const storedToken = localStorage.getItem("authToken");
-    const [careplans, setCarePlans] = useState([]);
+    const [careplans, setCarePlans] = useState(null);
 
     const getCarePlans = () => {
         axios
@@ -25,29 +27,150 @@ const CarePlans = () => {
     }, []);
 
 
-    // console.log(careplans)
+    console.log(careplans)
 
-    const calcToDoes = (date) => {
+  
+
+    const remindersPast = (date, propertyName, plantName, plantId) => {
         let now = new Date();
-    if (date < now) {
-     return "nothing to do to this plant";
-    } else if (date === now) {
-     return "Today you have to work on it a bit";
-    } else {
-     return (moment(date).startOf('day').fromNow() + " left until next step")   }}
- 
+        const formattedNow = moment(now).startOf('day');
+        const formattedDate = moment(date).startOf('day');
+
+        if (formattedDate.isBefore(formattedNow)) {
+
+            return (
+                <li className="py-4">
+                    <div className="flex items-center">
+
+                        <span className="text-lg font-medium pl-2 ">{propertyName} the <Link to={`/plants/${plantId}`} className="text-green-600 hover:text-green-800">{plantName}</Link> was done {moment(date).startOf('day').fromNow()}</span>
+
+                    </div>
+                </li>
+            )
+        }
+    }
+
+  
+
+    const calcToDoesToday = (date, propertyName, plantName, plantId) => {
+        let now = new Date();
+        const formattedNow = moment(now).startOf('day');
+        const formattedDate = moment(date).startOf('day');
+
+        if (formattedDate.isBefore(formattedNow)) {
+            return null;
+        }
+        else if (formattedDate.isSame(formattedNow)) {
+            return (
+                <li className="py-4">
+                    <div className="flex items-center">
+                        <label className="ml-3 block text-gray-900">
+                            <input type="checkbox" className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded" />
+                            <span className="text-lg font-medium pl-2">{propertyName} the <Link to={`/plants/${plantId}`} className="text-green-600 hover:text-green-800">{plantName}</Link>!</span>
+                        </label>
+                    </div>
+                </li>
+            )
+        }
+    }
+
+    const calcToDoesFuture = (date, propertyName, plantName, plantId) => {
+        let now = new Date();
+        const formattedNow = moment(now).startOf('day');
+        const formattedDate = moment(date).startOf('day');
+
+        if (formattedDate.isBefore(formattedNow)) {
+            return null;
+        }
+        else if (formattedDate.isSame(formattedNow)) {
+            return null;
+        }
+        return (
+            <li className="py-4">
+                <div className="flex items-center">
+                    <label className="ml-3 block text-gray-900"> <input type="checkbox" className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded" />
+                        <span className="text-lg font-medium pl-2">{formattedDate.fromNow() + ` you have to ${propertyName} the `} <Link to={`/plants/${plantId}`} className="text-green-600 hover:text-green-800">{plantName}</Link></span>
+                    </label>
+                </div>
+            </li>
+        )
+    }
 
     return (
-        <div className="careplans">
-            <h1>To DO</h1>
-            {/* <p>{careplans && calcToDoes(careplans.water)} </p> */}
-            {careplans  &&
-                    careplans.map(
-                        (obj) => (
-                            <div key={obj._id}>
-                                <p>{calcToDoes(obj.water)}</p>
-                            </div>
-                        ))}
+        <div className="careplans flex">
+
+            <div className="w-3/12 mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-16 ">
+                <div className="px-4 py-2">
+                    <h1 className="text-gray-800 font-bold text-2xl uppercase flex justify-center">Reminders ☝️</h1>
+                </div>
+
+                {careplans &&
+                    <>
+                        {careplans.some(careplan => remindersPast(careplan.water) || remindersPast(careplan.fertilize) || remindersPast(careplan.mist) || remindersPast(careplan.repot) || remindersPast(careplan.clean))
+                            ? (careplans.map((careplan, index) => (
+                                <ul className="divide-y divide-gray-200 px-4">
+                                    {remindersPast(careplan.water, "water", careplan.plant.name, careplan.plant._id)}
+                                    {remindersPast(careplan.fertilize, "fertilize", careplan.plant.name, careplan.plant._id)}
+                                    {remindersPast(careplan.mist, "mist", careplan.plant.name, careplan.plant._id)}
+                                    {remindersPast(careplan.repot, "repot", careplan.plant.name, careplan.plant._id)}
+                                    {remindersPast(careplan.clean, "clean", careplan.plant.name, careplan.plant._id)}
+                                    <hr />
+                                </ul>
+                            )))
+                            : 'You are not taking care of your plants!'
+                        }
+                    </>
+                }
+            </div>
+
+            <div className="w-3/12 mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-16 ">
+                <div className="px-4 py-2">
+                    <h1 className="text-gray-800 font-bold text-2xl uppercase flex justify-center">Today's To-Do List 📝</h1>
+                </div>
+
+                {careplans &&
+                    <>
+                        {careplans.some(careplan => calcToDoesToday(careplan.water) || calcToDoesToday(careplan.fertilize) || calcToDoesToday(careplan.mist) || calcToDoesToday(careplan.repot) || calcToDoesToday(careplan.clean))
+                            ? (careplans.map((careplan, index) => (
+                                <ul className="divide-y divide-gray-200 px-4">
+                                    {calcToDoesToday(careplan.water, "water", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesToday(careplan.fertilize, "fertilize", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesToday(careplan.mist, "mist", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesToday(careplan.repot, "repot", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesToday(careplan.clean, "clean", careplan.plant.name, careplan.plant._id)}
+                                    <hr />
+                                </ul>
+                            )))
+                            : 'Nothing to do today'
+                        }
+                    </>
+                }
+            </div>
+
+            <div className="w-3/12 mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-16 ">
+                <div className="px-4 py-2">
+                    <h1 className="text-gray-800 font-bold text-2xl uppercase flex justify-center">Next Day's To-Do List 🙏</h1>
+                </div>
+
+                {careplans &&
+                    <>
+                        {careplans.some(careplan => calcToDoesFuture(careplan.water) || calcToDoesFuture(careplan.fertilize) || calcToDoesFuture(careplan.mist) || calcToDoesFuture(careplan.repot) || calcToDoesFuture(careplan.clean))
+                            ? (careplans.map((careplan, index) => (
+                                <ul className="divide-y divide-gray-200 px-4">
+                                    {calcToDoesFuture(careplan.water, "water", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesFuture(careplan.fertilize, "fertilize", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesFuture(careplan.mist, "mist", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesFuture(careplan.repot, "repot", careplan.plant.name, careplan.plant._id)}
+                                    {calcToDoesFuture(careplan.clean, "clean", careplan.plant.name, careplan.plant._id)}
+                                    <hr />
+                                </ul>
+                            )))
+                            : 'Nothing to do the next days'
+                        }
+                    </>
+                }
+            </div>
+
         </div>
     );
 }
